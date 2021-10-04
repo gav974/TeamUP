7865ce71dcb79a6555e1f7a114c9429697e58135
gav974ordi1 <eclapier@msn.com>
2021-10-04 12:41:18 +0400

    first commit

---
+++ application/index.php
@@ -0,0 +1,26 @@
+<?php 
+require "../comfpl/main.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    require "../application/phpincludes/commonmeta.php";
+    require "../application/phpincludes/commoncss.php";
+    require "../application/phpincludes/theme.php";
+    require "../application/phpincludes/commonscript.php"; 
+    ?>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+require "../application/phpincludes/navbar.php"
+?>
+
+    <h1>Bienvenue</p>
+</body>
+</html>
